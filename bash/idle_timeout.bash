d3281ae6cb59f725d56923e26c4c1cc6b6e91e29
NatoTatoSHC <lesternate6@gmail.com>
2025-12-02 10:46:48 -0600

    Update idle_timeout.bash

--- bash/idle_timeout.bash
+++ bash/idle_timeout.bash
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
-# Set idle time in seconds
-IDLE_TIME=300
+IDLE_TIME=60           # seconds before running command
+COMMAND="am start -n com.example.aerial/.MainActivity"  # <-- replace with your screensaver app
+
+last_time=$(date +%s)
 
 while true; do
-    # Check for idle time
-    idle=$(xprintidle)
-    if [ "$idle" -ge $((IDLE_TIME * 1000)) ]; then
-        # Activate screensaver
-        xscreensaver-command -activate
+    sleep 1
+    now=$(date +%s)
+    elapsed=$((now - last_time))
+
+    if [ $elapsed -ge $IDLE_TIME ]; then
+        eval $COMMAND
+        last_time=$(date +%s)  # reset timer after running
     fi
-    sleep 10
 done
